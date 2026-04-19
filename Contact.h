6b23c9694db9e94c505d5e07b7e61d31b0b5bf4d
minhduc862005 <duchm9479@ut.edu.vn>
2026-04-19 18:13:44 +0700

    edit contact

--- Contact.h
+++ Contact.h
@@ -6,37 +6,23 @@
 using namespace std;
 
 enum Relationship {
-    FAMILY,
-    FRIEND,
-    TEACHER,
-    COLLEAGUE
+    FAMILY, FRIEND, TEACHER, COLLEAGUE
 };
 
 class Contact {
-private:
+public:
     string name;
     string phone;
     Relationship relation;
 
-public:
     // Constructor
-    Contact(string name = "", string phone = "", Relationship relation = FRIEND);
+    Contact(string n = "", string p = "", Relationship r = FRIEND);
 
-    // Getter
-    string getName();
-    string getPhone();
-    Relationship getRelation();
-
-    // Setter
-    void setName(string name);
-    void setPhone(string phone);
-    void setRelation(Relationship relation);
-
-    // Display
+    // Hàm hiển thị
     void display();
 };
 
-// Helper function
+// Hàm hỗ trợ
 string relationToString(Relationship r);
 
 #endif
